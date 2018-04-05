d90c42ed8f541f50f5ac72aeecfe885251dbf4a1
Othmani Ali <othmanixali@gmail.com>
2018-04-05 09:20:55 +0100

    imp BurgerIngredient in Burger

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
     return (
         <div class = {classes.Burger}>
-
+            <BurgerIngredient type = 'bread-top'/>
+            <BurgerIngredient type = 'cheese'/>
+            <BurgerIngredient type = 'meat'/>
+            <BurgerIngredient type = 'bread-bottom'/>
         </div>
     );
 }
