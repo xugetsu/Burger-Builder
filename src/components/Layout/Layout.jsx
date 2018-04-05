d2d3837ee591a86fd346d7f9fcce7d4eb9d7ed7b
Othmani Ali <othmanixali@gmail.com>
2018-04-05 07:47:35 +0100

    add styling in Layout cmp

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxx';
-
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main class = {classes.Content} >
             {props.children}
         </main>
     </Aux>
