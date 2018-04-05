fe1b773b262e21c16743f8e5986440c3d931818d
Othmani Ali <othmanixali@gmail.com>
2018-04-05 13:44:47 +0100

    adding no ingredient case

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,10 +3,14 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const ingredients = Object.keys(props.ingredients)
+    let ingredients = Object.keys(props.ingredients)
                         .map( igkey => [...Array(props.ingredients[igkey])].map( 
                                     (_,i) => <BurgerIngredient key = {igkey + i} type = {igkey}/> )
-                         );
+                         )
+                        .reduce((arr,el) => arr.concat(el), []);
+    if (!ingredients.length){
+        ingredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type = 'bread-top'/>
