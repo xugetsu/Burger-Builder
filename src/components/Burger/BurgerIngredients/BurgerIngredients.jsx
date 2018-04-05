8c174654d0c4dee6219272fd46bdc6a4cd74b192
Othmani Ali <othmanixali@gmail.com>
2018-04-05 08:24:47 +0100

    adding a dynamic burger Ingredient cmp

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.jsx
@@ -0,0 +1,37 @@
+import React , {Component} from 'react';
+import classes from './BurgerIngredients.css'
+
+class BurgerIngredients extends Component {
+    render(){
+        let ingredient = null;
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div class = {classes.BurgerIngredients}></div>;
+                break;
+            case ('bread-top'):
+                ingredient = (
+                    <div class = {classes.BreadTop} >
+                        <div class = {classes.Seeds1} ></div>
+                        <div class = {classes.Seeds2} ></div>
+                    </div>);   
+                break;
+            case ('meat'):
+                ingredient = <div class = {classes.Meat}></div>
+                break;
+            case ('cheese'):
+                ingredient = <div class = {classes.Cheese}></div>;
+                break;
+            case ('salad'):
+                ingredient = <div class = {classes.Salad}></div>;
+                break;
+            case ('bacon'):
+                ingredient = <div class = {classes.Bacon}></div>;
+                break;                        
+            default: 
+                ingredient = null;
+                break;
+        }
+        return ingredient;
+    };
+}
+export default BurgerIngredients;
