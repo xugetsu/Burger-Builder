0cbc21a0b6d089f7341ca82b93f3cd2d40765c72
Othmani Ali <othmanixali@gmail.com>
2018-04-05 09:11:21 +0100

    add Burger cmp

---
+++ src/components/Burger/Burger.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Burger.css'
+
+const burger = (props) => {
+    return (
+        <div class = {classes.Burger}>
+
+        </div>
+    );
+}
+
+export default burger;
