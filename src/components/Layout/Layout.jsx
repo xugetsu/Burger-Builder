cfab57c56e08e6c7587b9e201cbe657f4ab1cf1a
Othmani Ali <othmanixali@gmail.com>
2018-04-07 01:03:44 +0100

    changing Layout to a stateful cmp

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxx';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer open={true} closeSideDrawer={null}/>
-        <main className = {classes.Content} >
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component{
+    render(){
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer open={true} closeSideDrawer={null}/>
+                <main className = {classes.Content} >
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+}
 
-export default layout;
+export default Layout;
