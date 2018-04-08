1691a6668dcb0f23ff3a513182d08ecb9b7f185e
Othmani Ali <othmanixali@gmail.com>
2018-04-08 18:05:26 +0100

    create a hoc withErroHandler wrapper

---
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -0,0 +1,39 @@
+import React, {Component}  from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import A from '../Auxx';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error : null,
+        }
+        componentDidMount(){
+            axios.interceptors.request.use(req =>{
+                this.setState({error: null});
+                return req;
+            });
+            axios.interceptors.response.use(res => res, error =>{
+                this.setState({error: error});
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});            
+        }
+        render() {
+            return (
+                <A>
+                    <Modal 
+                        showModal ={this.state.error}
+                        modalClosed = {this.errorConfirmedHandler} >
+                        <p>something went wrong!</p>
+                        {this.state.error? this.state.error.message : null  }
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </A>
+            );
+        }
+    }
+}
+
+export default withErrorHandler;
