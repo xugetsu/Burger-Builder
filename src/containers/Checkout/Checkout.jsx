04f22f2c8e8276893cf5322c713f22b904931f7c
Othmani Ali <othmanixali@gmail.com>
2018-05-04 19:46:58 +0100

    Imp contact Data with Route in side Checkout cmp

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+import {Route} from 'react-router-dom';
 
 class Checkout extends Component{
     state = {
         ingredients: {},
+        totalPrice:0,
     }
     componentDidMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
-        for( let param of query){
-           ingredients[param[0]] = +param[1];
+        let totalPrice = 0;
+        for( let param of query){ // no need to add .entries()
+            if(param[0] === 'totalPrice'){
+                totalPrice = param[1]; 
+            }else{
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients:ingredients});
+        this.setState({ingredients:ingredients, totalPrice:totalPrice});
+        //console.log('[checkout]',this.props);
     }
     checkoutContinedHandler = () => {
         this.props.history.replace('/Checkout/contact-data');
@@ -20,13 +29,18 @@ class Checkout extends Component{
         this.props.history.goBack();
     }
     render(){
-        console.log(this.props);
         return(
             <div>
                 <CheckoutSummary 
                     ingredients={this.state.ingredients}
                     checkoutCanceled = {this.checkoutCanceledHandler}
                     checkoutContined = {this.checkoutContinedHandler}/>
+                <Route 
+                    path={this.props.match.url + '/contact-data'} 
+                    render ={(props) => <ContactData  ingredients={this.state.ingredients}    
+                                                      totalPrice={this.state.totalPrice}
+                                                      {...props}/>}
+                />
             </div>
         );
     }
