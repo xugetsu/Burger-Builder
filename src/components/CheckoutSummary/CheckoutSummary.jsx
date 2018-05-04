92ea40fe5a42427883b75a0269af8c81546b26a6
Othmani Ali <othmanixali@gmail.com>
2018-05-04 19:48:59 +0100

    Code opt in checkoutSummary cmp

--- src/components/CheckoutSummary/CheckoutSummary.jsx
+++ src/components/CheckoutSummary/CheckoutSummary.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import classes from './CheckoutSummary.css';
 import Burger from '../Burger/Burger';
 import Button from '../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
 
 const checkoutSummary = ( props ) => {
-    console.log(props);
+    //console.log(props);
 
     return(
         <div className={classes.CheckoutSummary} >
             <h1>We hope it taste good</h1> 
             <div className={classes.Burger}>
-                <Burger ingredients={props.ingredients? props.ingredients : {}} />
+                <Burger ingredients={props.ingredients} />
             </div>
             <Button btnType='Success' clicked={props.checkoutContined} >Continue</Button>
             <Button btnType='Danger' clicked={props.checkoutCanceled} >Cancel</Button>
