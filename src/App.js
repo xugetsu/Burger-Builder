e4d5c4d852a5e2426c3eb7d5982ca737b5d82f61
Othmani Ali <othmanixali@gmail.com>
2018-05-04 19:50:38 +0100

    Imp Orders with Routing inside App cmp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import {Route, Switch} from 'react-router-dom';
 import Checkout from '../src/containers/Checkout/Checkout';
+import Orders from '../src/containers/Orders/Orders';
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
           <Route path='/' exact component = {BurgerBuilder}/>
-          <Route path='/Checkout' exact component = {Checkout}/>
+          <Route path='/Checkout' component = {Checkout}/>
+          <Route path='/orders' component = {Orders}/>
         </Switch>
       </Layout>
     );
