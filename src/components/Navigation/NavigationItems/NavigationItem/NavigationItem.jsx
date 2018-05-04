a0047d5e183889d304c8ff89fd39258ae9531d35
Othmani Ali <othmanixali@gmail.com>
2018-05-04 20:04:08 +0100

    activeClassName bug fix in NavLink cmp

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -4,7 +4,10 @@ import {NavLink} from 'react-router-dom';
 
 const navigationItem = ( props ) => (
         <li className={classes.NavigationItem}>
-            <NavLink to={{pathname: props.link}}>{props.children}</NavLink>
+            <NavLink 
+                to={props.link}
+                exact = {props.exact}
+                activeClassName={classes.active} >{props.children}</NavLink>
         </li>
 );
 
